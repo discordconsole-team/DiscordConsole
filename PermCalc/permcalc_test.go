56f230c0309d262b8a817a227f907764d402a4ce
LEGOlord208 <LEGOlord208@gmail.com>
2017-05-04 06:28:34 +0200

    Removed tests again

--- PermCalc/permcalc_test.go
+++ PermCalc/permcalc_test.go
@@ -1,12 +1,7 @@
 package permcalc_test
 
-import (
-	"fmt"
-	"testing"
-
-	permcalc "github.com/legolord208/DiscordConsole/PermCalc"
-)
-
+/*
 func TestPermCalc(t *testing.T) {
 	fmt.Println(permcalc.PromptPerm())
 }
+*/
