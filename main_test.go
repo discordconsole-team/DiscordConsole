a3ef1ca9a2978587339f91f8cbb9e2e47548f8bc
LEGOlord208 <LEGOlord208@gmail.com>
2017-05-05 14:39:36 +0200

    Race condition tests

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/fatih/color"
+)
+
+func TestAlias(t *testing.T) {
+	// Alias known to use map
+	go func() {
+		command(nil, commandSource{
+		//	NoMutex: true,
+		}, "alias test exec echo hi", color.Output)
+	}()
+	go func() {
+		command(nil, commandSource{
+		//	NoMutex: true,
+		}, "test", color.Output)
+	}()
+}
