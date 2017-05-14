3828e39d7aaea75301441f2d41b1fa186cd42aef
LEGOlord208 <LEGOlord208@krake.one>
2017-05-14 15:20:43 +0200

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate cursive;
+
+use cursive::Cursive;
+use cursive::event::Key;
+use cursive::views::Dialog;
+use cursive::menu::MenuTree;
+
 fn main() {
-    println!("Hello, world!");
+	let mut screen = Cursive::new();
+	screen.add_global_callback('q', Cursive::quit);
+
+	screen.menubar()
+		.add_subtree("Guilds",
+			 MenuTree::new()
+				 .subtree("Example Server",
+					MenuTree::new()
+						.leaf("ID: 123456789", |_| {})
+						.leaf("Name: Example Server", |s| s.add_layer(Dialog::info("This would let you edit the name...")))
+				)
+		);
+
+	screen.add_layer(
+		Dialog::text("Press <esc> to access the menu, and <q> to quit")
+	);
+
+	screen.add_global_callback(Key::Esc, |s| s.select_menubar());
+	screen.set_autohide_menu(false);
+	screen.run();
 }
