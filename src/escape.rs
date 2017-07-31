4f56d38a1a94b639aaba714a9dbc96de621f5582
LEGOlord208 <LEGOlord208@krake.one>
2017-07-31 13:07:56 +0200

    with_capacity > new

--- src/escape.rs
+++ src/escape.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 pub fn escape(token: &str) -> String {
-	let mut escaped = String::new();
+	let mut escaped = String::with_capacity(token.len());
 	let mut found = false;
 	let mut empty = true;
 
