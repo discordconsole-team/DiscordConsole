5074f75ecbd40a1e717e4e7f882a59f40a13ee08
Martin Persson <mnpn03@gmail.com>
2018-08-06 00:20:20 +0200

    Fixed pchannels crash

--- commands_navigate.go
+++ commands_navigate.go
@@ -128,7 +128,13 @@ func commandsNavigate(session *discordgo.Session, cmd string, args []string, nar
 			table.AddRow()
 			recipient := ""
 			if len(channel.Recipients) < 1 {
-				recipient = channel.Recipients[0].Username
+				if len(channel.Recipients) != 0 {
+					recipient = channel.Recipients[0].Username
+				} else {
+					// Urgh, doesn't look like Discord returns
+					// anything other than [0/0].
+					recipient = tl("pointer.unknown")
+				}
 			}
 			kind := "DM"
 			if channel.Type == discordgo.ChannelTypeGroupDM {
