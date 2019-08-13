b37b2cc54bcdeb6981210923131477d0fdbeb1de
Martin Persson <mnpn03@icloud.com>
2019-08-13 16:35:04 +0200

    Update tab completes

--- languages.go
+++ languages.go
@@ -16,7 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 // TRANSLATORS:
-// - Swedish, Mnpn03
+// - English, jD91mZM2 & Mnpn
+// - Swedish, Mnpn
 // - Spanish, ArceCreeper
 package main
 
